02cd73d159558bb6641333730f6ff9419a2399e2
Villanelle1573 <3549778212@qq.com>
2024-04-25 16:15:36 +0800

    分开为组件

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,18 @@
 <template>
-  <div class="banner"></div>
-
-  <div class="container">
-    <h2 class="stage">专注中</h2>
-
-    <transition name="fade">
-      <h2 class="countdown" v-if="countdownSeconds > 0">{{ countdown }}</h2>
-      <h2 class="countdown" v-else>倒计时结束</h2>
-    </transition>
-
-    <div class="control">
-      <button class="start" @click="startCountdown">开始倒计时</button>
-      <button class="reset" @click="resetCountdown">结束倒计时</button>
-    </div>
-
-   
-  </div>
+  <Navigation />
+  <Clock />
 </template>
 
-<style>
-.banner {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 50px;
-  background-color: red;
-  color: #fff; /*文本颜色 */
-  padding: 10px 0;
-  text-align: center;
-  z-index: 1000; /* 可选：控制横幅在其他元素之上 */
-}
-
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 60px;
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
-  opacity: 0;
-}
-
-.control {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  width: 50%;
-  height: 32px;
-}
-
-.start {
-  width: 100px;
-  height: 30px;
-  margin-right: 10px;
-}
-
-.reset {
-  width: 100px;
-  height: 30px;
-  margin-left: 10px;
-}
-</style>
+<style></style>
 
 <script>
+import Navigation from "./components/Navigation.vue"
+import Clock from "./components/Clock.vue"
+
 export default {
-  data() {
-    return {
-      initialCountdownSeconds: 10,
-      countdownSeconds: 0,
-      countdownInterval: null
-    };
-  },
-
-  computed: {
-    countdown() {
-      const minutes = Math.floor(this.countdownSeconds / 60);
-      const seconds = this.countdownSeconds % 60;
-      return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    }
-  },
-
-  methods: {
-    startCountdown() {
-      this.countdownSeconds = this.initialCountdownSeconds;
-      clearInterval(this.countdownInterval); // 避免多次点击重复开始倒计时
-      this.countdownInterval = setInterval(() => {
-        if (this.countdownSeconds > 0) {
-          this.countdownSeconds--;
-        } else {
-          clearInterval(this.countdownInterval);
-        }
-      }, 1000);
-    },
-
-    resetCountdown() {
-      this.countdownSeconds = 0;
-      clearInterval(this.countdownInterval);
-    }
+  components: {
+    Navigation,
+    Clock
   }
 };
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="banner">
+        <div class="tabbox">
+            <h3>计时</h3>
+        </div>
+
+        <div class="tabbox">
+            <h3>日程</h3>
+        </div>
+
+        <div class="tabbox">
+            <h3>我的</h3>
+        </div>
+    </div>
+</template>
+
+<style>
+.banner {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    background-color: red;
+    color: #fff;
+    /*文本颜色 */
+    padding: 10px 0;
+    text-align: center;
+    z-index: 1000;
+    /* 可选：控制横幅在其他元素之上 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+}
+
+.tabbox {
+    width: 33.33%;
+    height: 70px;
+    border-left: #fff;
+    border-right: #fff;
+}
+</style>
+
+<script>
+
+</script>
